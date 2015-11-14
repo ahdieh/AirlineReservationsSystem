d894b19e9e1b946ffb5a75ca97d3784befbc465a
ahdieh <aemadi@my.centennialcollege.ca>
2015-11-14 03:57:57 -0600

    create the driver class

---
+++ src/AirlineReservationsTest.java
@@ -0,0 +1,13 @@
+
+
+public class AirlineReservationsTest {
+
+	public static void main(String[] args) {
+		Reservations reservations = new Reservations();
+		reservations.Selection();
+	
+		}
+
+	}
+
+
